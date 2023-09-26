1940d3eecc0d3b0dda6d0cdc9f769ffbff86d9ff
Gowdham <gowdhamnagesh@gmail.com>
2023-09-26 02:24:01 -0400

    code push

---
+++ src/org/samp/Test.java
@@ -0,0 +1,18 @@
+package org.samp;
+
+public class Test {
+	
+	public void employee() {
+		
+		System.out.println("Employee name is : Gowdham. E");
+		
+	}
+
+	public static void main(String[] args) {
+		
+		Test t = new Test();
+		t.employee();
+	}
+
+}
+
